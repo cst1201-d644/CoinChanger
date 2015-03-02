9c625fa259d7609ed73035b32651aa8ee69072c0
class <class@CST_N417_21.cst.citytech.cuny.edu>
2015-03-02 16:31:02 -0500

    LAB2

---
+++ src/lab/pkg2/Lab2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author class
+ */
+public class Lab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner Keyboard = new Scanner(System.in);
+        int cents, quarters, dimes, nickels, pennies;
+        System.out.print("Enter amount of change in cents:");
+        cents = Keyboard.nextInt();
+        quarters = (cents / 25);
+        System.out.println("quarters=" + quarters);
+        dimes = (cents % 25) / 10;
+        System.out.println("dimes=" + dimes);
+        nickels = ((cents % 25) / 10) % 5;
+        System.out.println("nickels=" + nickels);
+        pennies = cents - quarters * 25 - dimes * 10 - nickels * 5;
+        System.out.println("pennies=" + pennies);
+    }
+
+}
