2874edd262a6cebeabd91d33f09360234ae94745
rkhatchadourian <rkhatchadourian@CST_N417_25.cst.citytech.cuny.edu>
2015-03-02 16:54:24 -0500

    Initial code.

--- src/lab2/CoinChanger.java
+++ src/lab2/CoinChanger.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package lab2;
 
 /**
@@ -12,11 +6,14 @@ package lab2;
  */
 public class CoinChanger {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO: Ask the user for D.
+        int d;
+        
+        // TODO: Compute the amount of change.
+        int quarters = 0;
+        int dimes = 0;
+        int nickels = 0;
     }
     
 }
