127a78a3ee98742a496d7d21bbdfd00a21ab3e4e
class <class@CST_N417_21.cst.citytech.cuny.edu>
2015-03-02 17:20:25 -0500

    Lab 2 fixed (=)

--- src/lab/pkg2/Lab2.java
+++ src/lab/pkg2/Lab2.java
@@ -17,28 +17,27 @@ public class Lab2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner Keyboard = new Scanner(System.in);
-        int cents, quarters, dimes, nickels, pennies, totalcoin,quarterscent,dimescent,nickelscents;
+        Scanner Keyboard = new Scanner(System.in);//import the Scanner
+        int cents, quarters, dimes, nickels, pennies, totalcoin, quarterscent, dimescent, nickelscents;
         System.out.print("Enter amount of change in cents:");//Ask for user to enter how many cents
-        cents = Keyboard.nextInt();//the input are in this variable
-        if (cents>=0){//currency must be positive
-        quarters = (cents / 25);
-        quarterscent=quarters*25;
-        System.out.println("quarters: " + quarters+" (="+quarterscent+")");
-        dimes = (cents % 25) / 10;
-        dimescent=dimes*10;
-        System.out.println("dimes: " + dimes+" (="+dimescent+")");
-        nickels = ((cents % 25) % 10) / 5;
-        nickelscents=nickels*5;
-        System.out.println("nickels: " + nickels+" (="+nickelscents+")");
-        pennies = cents - quarters * 25 - dimes * 10 - nickels * 5;
-        System.out.println("pennies: " + pennies+" (="+pennies+")");
-        totalcoin = quarters + dimes + nickels + pennies;
-        System.out.println("Total Number of Coins: " + totalcoin);
-        }else{
+        cents = Keyboard.nextInt();
+        if (cents >= 0) {
+            quarters = (cents / 25);
+            quarterscent = quarters * 25;
+            System.out.println("quarters: " + quarters + " (=" + quarterscent + ")");
+            dimes = (cents % 25) / 10;
+            dimescent = dimes * 10;
+            System.out.println("dimes: " + dimes + "    (=" + dimescent + ")");
+            nickels = ((cents % 25) % 10) / 5;
+            nickelscents = nickels * 5;
+            System.out.println("nickels: " + nickels + "  (=" + nickelscents + ")");
+            pennies = cents - quarters * 25 - dimes * 10 - nickels * 5;
+            System.out.println("pennies: " + pennies + "  (=" + pennies + ")");
+            totalcoin = quarters + dimes + nickels + pennies;
+            System.out.println("Total Number of Coins: " + totalcoin);
+        } else {
             System.out.println("Coin can't be negative");
         }
-                
+
     }
 }
-
