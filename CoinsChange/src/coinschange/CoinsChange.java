083ec59dfc628e5f25a8390547b6a27e20bfbaa6
class <class@CST_N417_15.cst.citytech.cuny.edu>
2015-03-02 16:27:48 -0500

    Started

---
+++ CoinsChange/src/coinschange/CoinsChange.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package coinschange;
+
+/**
+ *This is the best program to calculate coins change
+ *
+ * @author Francisco Latacela   Francisco.latacela@mail.citytech.cuny.edu
+ * @version 03/04/2015
+ */
+public class CoinsChange {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
