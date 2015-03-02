1256d6b4b684a47e5bad0887f9275baeaa2bc1e5
class <class@CST_N417_13.cst.citytech.cuny.edu>
2015-03-02 17:24:18 -0500

    labs222

--- src/lab2/CoinChanger.java
+++ src/lab2/CoinChanger.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package lab2;
 
+import java.util.Scanner;
+
 /**
+ * comment
  *
- * @author class
+ * @author mingjiehuang
  */
 public class CoinChanger {
 
@@ -16,7 +18,18 @@ public class CoinChanger {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Enter the amount of change.");
+        Scanner scan = new Scanner(System.in);
+        int D = scan.nextInt();
+
+        int quarters = D/25;
+        D=D% 25;
+        int dimes = D/10;
+        D=D%10;
+        int nickels = D/5;
+        D=D%5;
+        
+       
     }
-    
+
 }
