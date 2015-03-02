8de665cf990139207b67f0a34950c42b44d5d05d
class <class@CST_N417_19.cst.citytech.cuny.edu>
2015-03-02 17:32:57 -0500

    undone lab2

--- lab 2/src/lab/pkg2/Lab2.java
+++ lab 2/src/lab/pkg2/Lab2.java
@@ -6,6 +6,8 @@
 
 package lab.pkg2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author class
@@ -17,6 +19,50 @@ public class Lab2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        // TODO:ask the user for D.
+        int d = 0;
+        // TODO:compute the amount of change.
+        // TODO:calculate the number of Quarters.
+        int quarters;
+        int quartersR;
+        int quarter_value = 25;
+        // TODO:calculate the number of Dinnes.
+        int dinnes;
+        int dinnesR;
+        int dinnes_value = 10;
+        // TODO:calculate the number of Nickels.
+        int nickels;
+        int nickelsR;
+        int nickels_value = 5;
+        // TODO:calculate the number of Pennies.
+        int pennies;
+        int penniesR;
+        int pennies_value = 1;
+
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.println("Input the D: ");
+        String number = inputScanner.nextLine();
+
+        quarters = d / quarter_value;
+        System.out.println("quarters are: " + quarters);
+        quartersR = d % quarter_value;
+        
+
+        dinnes = quartersR / dinnes_value;
+        System.out.println("dinnes are: " + dinnes);
+        dinnesR = quartersR % dinnes_value;
+        
+
+        nickels = dinnesR / nickels_value;
+        System.out.println("nickels are: " + nickels);
+        nickelsR = dinnesR % nickels_value;
+        
+
+        pennies = nickelsR / pennies_value;
+        System.out.println("pennies are: " + pennies);
+
     }
-    
+
 }
