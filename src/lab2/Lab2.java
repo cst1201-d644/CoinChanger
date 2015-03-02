27bfbbc146228c697428b42ec2f2ddcfe2163be6
class <class@CST_N417_20.cst.citytech.cuny.edu>
2015-03-02 17:34:55 -0500

    Starting lab2

--- src/lab2/Lab2.java
+++ src/lab2/Lab2.java
@@ -6,6 +6,8 @@
 
 package lab2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author class
@@ -16,7 +18,27 @@ public class Lab2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic her
+    // Store variable for total amount of change.    
+    int d;
+    // Store variable for amount of quarters.
+    int quarters;
+    // Store variable for amount of dimes.
+    int dimes;
+    // Store variable for amount of nickels.
+    int nickels;
+    // Store variable for amount of pennies.
+    int pennies;
+    // Variable to get user input
+    Scanner inputScanner = new Scanner(System.in);
+   String input = inputScanner.nextLine();
+    // Question for user to get input
+    System.out.println("Enter the total amount of change: ");
+    // Algorithm for Quarters
+    int d = input.get();
+    quarters = d / 25;
+    dimes = (d % quarters);
+    nickels = (d % dimes);
+    pennies = (d % nickels);
+    System.out.println("Quarters: " + quarters);
     }
-    
 }
