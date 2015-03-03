18ebaf4da4e7fd77d49549599110c3d3f6b9a781
Winnie <Winnie@Winnie-PC>
2015-03-03 16:50:34 -0500

    Completed Lab 2.

--- src/lab2/CoinChanger.java
+++ src/lab2/CoinChanger.java
@@ -1,16 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package lab2;
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 /**
- *
- * @author 15320635
+ * Lab 2: Coin Changer 
+ * 
+ * @author Winnie Wu and Kenny Tsang
+ * @version 03/02/2015
  */
 public class CoinChanger {
 
@@ -21,34 +18,42 @@ public class CoinChanger {
         final int QUARTER_VALUE = 25;
         final int DIME_VALUE = 10;
         final int NICKEL_VALUE = 5;
+        final int PENNY_VALUE = 1;
         
         // TODO: Ask the user for D.
-        String input = JOptionPane.showInputDialog("Enter the amount in change: ");
-        int D = Integer.parseInt(input);
+        System.out.println("Enter amount in change: ");
+        Scanner input = new Scanner(System.in);
+        int D = input.nextInt();
         
         // TODO: Compute the amount of change.
         // TODO: Calculate the number of quarters.
         int quarterCount = D / QUARTER_VALUE;
         D %= QUARTER_VALUE;
-        System.out.println("I now have " + quarterCount + " quarters.");
         
         // TODO: Calculate the number of dimes.
         int dimeCount = D / DIME_VALUE;
         D %= DIME_VALUE; 
-        System.out.println("I now have " + dimeCount + " dimes.");
-        
+               
         // TODO: Calculate the number of nickels.
         int nickelCount = D / NICKEL_VALUE;
         D %= NICKEL_VALUE;
-        System.out.println("I now have " + nickelCount + " nickels.");
-        
+              
         // TODO: What's left in D is the amount of pennies.
         int pennyCount = D;
-        System.out.println("I now have " + pennyCount + " pennies.");
-        
+              
         // TODO: Print the number of coins in each category.
-        JOptionPane.showMessageDialog("Quarters: " + quarterCount +
-        // TODO: Print the total number of coins. 
+        System.out.println("Quarters: " + quarterCount + 
+                " (= " + quarterCount * QUARTER_VALUE + ")");
+        System.out.println("Dimes: " + dimeCount + 
+                " (= " + dimeCount * DIME_VALUE + ")");
+        System.out.println("Nickels: " + nickelCount + 
+                " (= " + nickelCount * NICKEL_VALUE + ")");
+        System.out.println("Pennies: " + pennyCount + 
+                " (= " + pennyCount * PENNY_VALUE + ")");
+       
+        // TODO: Print the total number of coins.
+        int sum = quarterCount + dimeCount + nickelCount + pennyCount;
+        System.out.println("Total number of coins: " + sum);
         
     }
     
