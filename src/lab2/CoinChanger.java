2ecf68afa738bd17dc3092f38d83a7b9b1c28cee
class <class@CST_N417_07.cst.citytech.cuny.edu>
2015-03-04 16:12:07 -0500

    Removed TODO comments.

--- src/lab2/CoinChanger.java
+++ src/lab2/CoinChanger.java
@@ -20,28 +20,24 @@ public class CoinChanger {
         final int NICKEL_VALUE = 5;
         final int PENNY_VALUE = 1;
         
-        // TODO: Ask the user for D.
+        
         System.out.println("Enter amount in change: ");
         Scanner input = new Scanner(System.in);
         int D = input.nextInt();
         
-        // TODO: Compute the amount of change.
-        // TODO: Calculate the number of quarters.
+        
         int quarterCount = D / QUARTER_VALUE;
         D %= QUARTER_VALUE;
         
-        // TODO: Calculate the number of dimes.
         int dimeCount = D / DIME_VALUE;
         D %= DIME_VALUE; 
                
-        // TODO: Calculate the number of nickels.
         int nickelCount = D / NICKEL_VALUE;
         D %= NICKEL_VALUE;
               
-        // TODO: What's left in D is the amount of pennies.
         int pennyCount = D;
               
-        // TODO: Print the number of coins in each category.
+
         System.out.println("Quarters: " + quarterCount + 
                 " (= " + quarterCount * QUARTER_VALUE + ")");
         System.out.println("Dimes: " + dimeCount + 
@@ -51,7 +47,7 @@ public class CoinChanger {
         System.out.println("Pennies: " + pennyCount + 
                 " (= " + pennyCount * PENNY_VALUE + ")");
        
-        // TODO: Print the total number of coins.
+      
         int sum = quarterCount + dimeCount + nickelCount + pennyCount;
         System.out.println("Total number of coins: " + sum);
         
